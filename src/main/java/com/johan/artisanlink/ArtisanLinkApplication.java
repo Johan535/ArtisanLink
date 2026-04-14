5bc419f90ab7f7a75caba8525222cf7199d38f1f
嘎嘎嘎 <wjh13216800080@qq.com>
2026-04-14 16:59:02 +0800

    依赖配置与项目搭建

---
+++ src/main/java/com/johan/artisanlink/ArtisanLinkApplication.java
@@ -0,0 +1,13 @@
+package com.johan.artisanlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtisanLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johan/artisanlink/ArtisanLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johan.artisanlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
